58430142aac2e54d40b3ae0399d58e48c56d53be
Theo Sementa <theosementa@gmail.com>
2025-01-09 14:09:47 +0100

    feat: refactor rank

--- domain/models/Warrior.ts
+++ domain/models/Warrior.ts
@@ -17,23 +17,25 @@ export class Warrior {
       case (this.level >= 1 && this.level <= 9):
         return Rank.PUSHOVER
       case (this.level >= 10 && this.level <= 19):
-        return Rank.PUSHOVER
+        return Rank.NOVICE
       case (this.level >= 20 && this.level <= 29):
-        return Rank.PUSHOVER
+        return Rank.FIGHTER
       case (this.level >= 30 && this.level <= 39):
-        return Rank.PUSHOVER
+        return Rank.WARRIOR
       case (this.level >= 40 && this.level <= 49):
-        return Rank.PUSHOVER
+        return Rank.VETERAN
       case (this.level >= 50 && this.level <= 59):
-        return Rank.PUSHOVER
+        return Rank.SAGE
       case (this.level >= 60 && this.level <= 69):
-        return Rank.PUSHOVER
+        return Rank.ELITE
       case (this.level >= 70 && this.level <= 79):
-        return Rank.PUSHOVER
+        return Rank.CONQUEROR
       case (this.level >= 80 && this.level <= 89):
-        return Rank.PUSHOVER
-      case (this.level >= 90):
-        return Rank.PUSHOVER
+        return Rank.CHAMPION
+      case (this.level >= 90 && this.level <= 99):
+        return Rank.MASTER
+      case (this.level == 100):
+        return Rank.GREATEST
       default:
         return Rank.PUSHOVER
     }
