372131357f05808e567a546289e1485e8434dcea
Theo Sementa <theosementa@gmail.com>
2025-01-09 13:42:42 +0100

    first commit

---
+++ Rank.ts
@@ -0,0 +1,13 @@
+export enum Rank {
+  PUSHOVER = 'Pushover',
+  NOVICE = 'Novice',
+  FIGHTER = 'Fighter',
+  WARRIOR = 'Warrior',
+  VETERAN = 'Veteran',
+  SAGE = 'Sage',
+  ELITE = 'Elite',
+  CONQUEROR = 'Conqueror',
+  CHAMPION = 'Champion',
+  MASTER = 'Master',
+  GREATEST = 'Greatest'
+}
---
+++ main.ts
@@ -0,0 +1,5 @@
+function main() {
+  console.log('coucou');
+}
+
+main()
